c2e409213ceb07b210178f3117babafdf8a46229
xerotresdin <wli111@jh.edu>
2024-03-13 21:16:50 -0400

    re-fresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { log } from "@/lib/logger";
 
 function App() {
-  log.debug("Hello, world!");
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Button>Welcome to Winston's Best Self Portfolio!</Button>
