d84ac172aba90dd520db60acc49f9ee65581222e
xerotresdin <wli111@jh.edu>
2024-03-13 21:11:06 -0400

    Re-fix template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
-const typographyPlugin = require('@tailwindcss/typography');
-const typographyStyles = require('./typography');
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'selector',
+  darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   prefix: "",
   theme: {
@@ -18,22 +16,6 @@ module.exports = {
         "2xl": "1400px",
       },
     },
-    fontSize: {
-      xs: ['0.8125rem', { lineHeight: '1.5rem' }],
-      sm: ['0.875rem', { lineHeight: '1.5rem' }],
-      base: ['1rem', { lineHeight: '1.75rem' }],
-      lg: ['1.125rem', { lineHeight: '1.75rem' }],
-      xl: ['1.25rem', { lineHeight: '2rem' }],
-      '2xl': ['1.5rem', { lineHeight: '2rem' }],
-      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
-      '4xl': ['2rem', { lineHeight: '2.5rem' }],
-      '5xl': ['3rem', { lineHeight: '3.5rem' }],
-      '6xl': ['3.75rem', { lineHeight: '1' }],
-      '7xl': ['4.5rem', { lineHeight: '1' }],
-      '8xl': ['6rem', { lineHeight: '1' }],
-      '9xl': ['8rem', { lineHeight: '1' }],
-    },
-    typography: typographyStyles,
     extend: {
       colors: {
         border: "hsl(var(--border))",
@@ -91,8 +73,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    typographyPlugin,
-  ],
-};
+  plugins: [require("tailwindcss-animate")],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/mybestselfportfolio",
+  base: "mybestselfportfolio",
   plugins: [react()],
   resolve: {
     alias: {
