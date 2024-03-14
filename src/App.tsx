5ab16b65ee43a6a9d9b66d805ff0ea9b306622c7
xerotresdin <wli111@jh.edu>
2024-03-13 20:12:39 -0400

    Ready for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Button } from "./components/ui/button";
+import { Button } from "@/components/ui/button";
+import { log } from "@/lib/logger";
 
 function App() {
+  log.debug("Hello, world!");
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <Button>Welcome to my Best Self Portfolio!</Button>
+      <Button>Hello, world!</Button>
     </div>
   );
 }
