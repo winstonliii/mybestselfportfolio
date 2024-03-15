ef3173973238e1b75eb4db3ffa3efce7534ef931
xerotresdin <wli111@jh.edu>
2024-03-14 21:34:46 -0400

    NavBar on landing fixed

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,30 @@ import Assessments from './App/Assessments';
 import Growth from './App/Growth';
 import Interviews from './App/Interviews'; 
 import Reflections from './App/Reflections';
+import Landing from "./App/Landing";
 
 function App() {
   return (
+    <div>
     <Router>
       <div>
         <NavBar />
+        
         <Routes>
-          <Route path="/mybestselfportfolio/home" element={<Home />} />
+          <Route path="/mybestselfportfolio/home" element={<><Landing /><Home /></>} />
           <Route path="/mybestselfportfolio/about-me" element={<AboutMe />} />
           <Route path="/mybestselfportfolio/interviews" element={<Interviews />} />
           <Route path="/mybestselfportfolio/assessments" element={<Assessments />} />
           <Route path="/mybestselfportfolio/reflections" element={<Reflections />} />
           <Route path="/mybestselfportfolio/growth" element={<Growth />} />
-          {/* Redirect base URL to Home */}
-          <Route path="/" element={<Home />} />
-          {/* You can add a catch-all route for 404 not found pages */}
+          <Route path="/mybestselfportfolio" element={<><Landing /><Home /></>} />
+         
         </Routes>
+        
       </div>
     </Router>
+
+    </div>
   );
 }
 
